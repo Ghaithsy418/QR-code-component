610433814e0a77414416cd99aaa8dc1976aceaa4
Ghaith Shabakji <ghaithsy418@gmail.com>
2025-01-13 13:21:13 +0300

    small nice component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <main className="card">
+      <img src="./images/image-qr-code.png" alt="QR-code" className="qr" />
+      <div className="margin">
+        <h1 className="title">
+          Improve your front-end skills by building projects
+        </h1>
+        <p className="text">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
